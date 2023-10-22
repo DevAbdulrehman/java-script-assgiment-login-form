184c9f151c5f2c4d44e1ae960c1307d63de42c97
DevAbdulrehman <ahmedrehman865@gmail.com>
2023-10-22 23:40:22 +0500

    assignment completed

---
+++ script.js
@@ -0,0 +1,29 @@
+let users = [];
+
+function register() {
+  users.push({
+    firstName: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    pass: document.getElementById("pass").value,
+  });
+  console.log(users);
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("pass").value = "";
+}
+
+function login() {
+  let loginEmail = document.getElementById("login-email").value;
+  let loginPass = document.getElementById("login-pass").value;
+  for (let i = 0; i < users.length; i++) {
+    if (loginEmail == users[i].email) {
+      if (loginPass == users[i].pass) {
+        window.alert("succesfully login");
+      } else {
+        window.alert("Please enter correct email or password");
+      }
+    }
+  }
+  document.getElementById("login-email").value = "";
+  document.getElementById("login-pass").value = "";
+}
